0d2aede562dcfb25a839b62f54a6c062d107dd20
Roberto Cifuentes <rcifuentesv90@gmail.com>
2021-08-29 18:10:30 -0400

    feat: add create to posts

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,4 +1,26 @@
 class PostController < ApplicationController
   def index
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to :root, notice: "Post was successfully created." }
+        format.json { render :root, status: :created, location: @post }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+
+  end
+  private
+    def post_params
+      params.require(:post).permit(:description, :title)
+    end
 end
