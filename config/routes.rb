ae08c266be6f61db843313107b270a01893fb3ce
Roberto Cifuentes <rcifuentesv90@gmail.com>
2021-08-28 22:07:13 -0400

    feat: add posts controller, add default route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 #
 
 Rails.application.routes.draw do
+  root 'post#index'
+  get 'post/index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
