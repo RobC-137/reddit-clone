249f5803f1e72bb01e9684c6928a7ca0e817a2e2
Roberto Cifuentes <rcifuentesv90@gmail.com>
2021-09-03 16:04:21 -0400

    feat: add username to signup
    Added devise controllers to be able to support more attributes, username added to signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,5 +24,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
   has_many :posts
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'post/new'
   post 'post/create'
   get '/post/:id', to: 'post#show', as: 'post'
-  devise_for :users
+  # devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, path: "", controllers: { sessions: "users/sessions", registrations: "users/registrations" }, path_names: { sign_in: 'login', password: 'forgot', confirmation: 'confirm', unlock: 'unblock', sign_up: 'register', sign_out: 'signout'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
